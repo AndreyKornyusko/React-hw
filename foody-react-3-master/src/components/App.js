cec8495c814c7f7290ee658ae551e953a90cdb9f
Andrey Kornyushko <kap86@ukr.net>
2018-11-29 19:02:11 +0200

    added hw1

---
+++ foody-react-3-master/src/components/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import AppHeader from './AppHeader';
+import orderJson from '../json/order-history.json';
+import menuJson from '../json/menu.json';
+import OrderHistory from './OrderHistory';
+import DishMenu from './DishMenu';
+import MenuFilter from './MenuFilter';
+import CommentsList from './CommentsList';
+import CommentsForm from './CommentsForm';
+
+const filterMenuDishes = (filter, dishes) =>
+  dishes.filter(dish => dish.name.toLowerCase().includes(filter.toLowerCase()));
+
+export default class App extends Component {
+  state = {
+    filter: '',
+    notes: [],
+  };
+
+  handleAddNote = (rating, text) => {
+    this.setState(prevState => ({
+      notes: [{ id: Date.now(), rating, text }, ...prevState.notes],
+    }));
+  };
+
+  handleFilterChange = e => {
+    // console.log(e.target.value);
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  render() {
+    // console.log('App state', this.state);
+
+    const { filter } = this.state;
+    // console.log('filter', filter);
+
+    const filteredDishes = filterMenuDishes(filter, menuJson);
+
+    return (
+      <div>
+        <AppHeader />
+        <OrderHistory items={orderJson} />
+
+        <MenuFilter filter={filter} onFilterChange={this.handleFilterChange} />
+
+        <DishMenu items={filteredDishes} />
+        <CommentsForm onSubmit={this.handleAddNote} />
+        <CommentsList notes={this.state.notes} />
+      </div>
+    );
+  }
+}
